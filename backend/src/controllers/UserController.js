747a568bb49429ff65f7248bda1bfd02550225c3
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2021-08-15 14:14:17 -0300

    Finishing all crud operations

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -4,28 +4,47 @@ module.exports = {
   async store(req, res) {
     const data = req.body;
 
-    const userExists = await User.findOne({ where: {email: data.email}})
+    const userExists = await User.findOne({ where: { email: data.email }})
         
-    if(userExists){
-      return res.status(400).json({ error: "User already exists"})
-    }
+    if(userExists) res.status(400).json({ error: "User already exists"});
 
     await User.create(data);
 
     return res.json(data);
   },
 
+  async list(req, res){
+    const users = await User.findAll();
+
+    return res.json(users)
+  },
+
   async update(req, res) {
     const data = req.body;
+    const { id } = req.params;
 
-    const user = await User.findByPk(req.userId)
-        
-    if(userExists){
-      return res.status(400).json({ error: "User already exists"})
+    const user = await User.findByPk(id);
+    
+    if (user.email !== data.email) {
+      const userExists = await User.findOne({ where: { email: data.email}});
+
+      if(userExists) res.status(400).json({ error: "E-mail already in use"});    
     }
 
     await user.update(data);
 
     return res.json(data);
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+ 
+    const user = await User.findByPk(id);
+
+    if (!user) res.status(400).json({ error: "User does not exists"});
+
+    await user.destroy();
+
+    return res.json(user);
   }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,5 +4,8 @@ const UserController = require("./controllers/UserController");
 const routes = express.Router();
 
 routes.post("/users", UserController.store);
+routes.get("/users", UserController.list);
+routes.put("/users/:id", UserController.update);
+routes.delete("/users/:id", UserController.delete);
  
 module.exports = routes;
